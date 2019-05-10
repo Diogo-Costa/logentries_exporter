5ef1ec457a78776696dd7abc313a087ac2bb7c03
Diogo Costa <diogo.fe.costa@gmail.com>
2019-05-10 18:28:54 -0300

    Fixes url api

---
+++ exporter/logs_usage.go
@@ -0,0 +1,161 @@
+package exporter
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+
+	"encoding/json"
+)
+
+// Structs
+type responseLogsStruct struct {
+	PerDayUsage struct {
+		Period struct {
+			From string `json:"from"`
+			To   string `json:"to"`
+		} `json:"period"`
+		ReportInterval string `json:"report_interval"`
+		UsageUnits     string `json:"usage_units"`
+		Usage          []struct {
+			Interval string `json:"interval"`
+			LogUsage []struct {
+				ID    string `json:"id"`
+				Usage int    `json:"usage"`
+			} `json:"log_usage"`
+		} `json:"usage"`
+	} `json:"per_day_usage"`
+}
+
+//
+type responseLogStesStruct struct {
+	Logs []struct {
+		ID         string        `json:"id"`
+		Name       string        `json:"name"`
+		Tokens     []string      `json:"tokens"`
+		Structures []interface{} `json:"structures"`
+		UserData   struct {
+		} `json:"user_data,omitempty"`
+		SourceType      string      `json:"source_type"`
+		TokenSeed       interface{} `json:"token_seed"`
+		RetentionPeriod string      `json:"retention_period"`
+		Links           []struct {
+			Rel  string `json:"rel"`
+			Href string `json:"href"`
+		} `json:"links"`
+		LogsetsInfo []struct {
+			ID    string `json:"id"`
+			Name  string `json:"name"`
+			Links []struct {
+				Rel  string `json:"rel"`
+				Href string `json:"href"`
+			} `json:"links"`
+		} `json:"logsets_info"`
+	} `json:"logs"`
+}
+
+// LogsStruct is return for Colletor prometheus
+type LogsStruct struct {
+	APIKEY      string
+	mutex       sync.Mutex
+	client      *http.Client
+	periodUsage *prometheus.Desc
+}
+
+// LogsGetUsage returns an initialized Exporter.
+func LogsGetUsage(apikey string) *LogsStruct {
+	return &LogsStruct{
+		APIKEY: apikey,
+		periodUsage: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "log_usage_daily"),
+			"Log Usage Size in bytes (d-1)",
+			[]string{"logID", "logName", "logSet"},
+			nil),
+		client: &http.Client{},
+	}
+}
+
+// Describe describes all the metrics ever exported by the logentries exporter. It
+// implements prometheus.Collector.
+func (e *LogsStruct) Describe(ch chan<- *prometheus.Desc) {
+	ch <- e.periodUsage
+}
+
+func requestHTTP(url, apikey string) *http.Response {
+	// Build the request
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("x-api-key", apikey)
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+	}
+	if resp.StatusCode >= 200 && resp.StatusCode <= 299 {
+		log.Debugln("Logs Status Code:", resp.StatusCode)
+	} else {
+		log.Errorln("Logs Status Code:", resp.StatusCode)
+
+	}
+
+	return resp
+}
+
+// Collect fetches the stats from configured location and delivers them
+// as Prometheus metrics.
+func (e *LogsStruct) collect(ch chan<- prometheus.Metric) error {
+	log.Debugln("---------------------- SCRAPER LOGS SIZE ----------------------------------")
+
+	// Create parse url to capture all sizes logs
+	parseLogsURL := "https://eu.rest.logs.insight.rapid7.com/usage/organizations/logs?time_range=yesterday&interval=day"
+	log.Debugln(parseLogsURL)
+	responseLogs := requestHTTP(parseLogsURL, e.APIKEY)
+	defer responseLogs.Body.Close()
+	var recordLogsStruct responseLogsStruct
+	if err := json.NewDecoder(responseLogs.Body).Decode(&recordLogsStruct); err != nil {
+		log.Fatal(err)
+	}
+
+	// Create parse url to capture all logs in account
+	parseLogSetsURL := "https://eu.rest.logs.insight.rapid7.com/management/logs"
+	log.Debugln(parseLogSetsURL)
+	responseLogStes := requestHTTP(parseLogSetsURL, e.APIKEY)
+	defer responseLogStes.Body.Close()
+	var recordLogStesStruct responseLogStesStruct
+	if err := json.NewDecoder(responseLogStes.Body).Decode(&recordLogStesStruct); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, logs := range recordLogsStruct.PerDayUsage.Usage {
+		for _, logUsage := range logs.LogUsage {
+			for _, logName := range recordLogStesStruct.Logs {
+				for _, logSetName := range logName.LogsetsInfo {
+					if logUsage.ID == logName.ID {
+						log.Debugln("LogStet: ", logSetName.Name)
+						log.Debugln("ID: ", logUsage.ID)
+						log.Debugln("Name: ", logName.Name)
+						log.Debugln("Size: ", logUsage.Usage)
+						ch <- prometheus.MustNewConstMetric(e.periodUsage, prometheus.GaugeValue, float64(logUsage.Usage), logUsage.ID, logName.Name, logSetName.Name)
+					}
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// Collect fetches the stats from configured logentries location and delivers them
+// as Prometheus metrics.
+// It implements prometheus.Collector.
+func (e *LogsStruct) Collect(ch chan<- prometheus.Metric) {
+	e.mutex.Lock() // To protect metrics from concurrent collects.
+	defer e.mutex.Unlock()
+	if err := e.collect(ch); err != nil {
+		log.Errorf("Error scraping logentries: %s", err)
+	}
+	return
+}
--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 var (
 	listeningAddress = flag.String("telemetry.address", ":9578", "Address on which to expose metrics.")
 	metricsPath      = flag.String("telemetry.endpoint", "/metrics", "Path under which to expose metric.")
-	service          = flag.String("service", "", "Select service for scraper (logentries or rapid7).")
-	logentriesID     = flag.String("logentriesID", "", "ID Account to logentries metrics.")
 	apikey           = flag.String("apikey", "", "APIKEY to connect logentries metrics.")
 	isDebug          = flag.Bool("isDebug", false, "Output verbose debug information.")
 )
@@ -36,22 +34,15 @@ func main() {
 		log.Fatal("Cannot specify both apikey")
 	}
 
-	if *service == "logentries" {
-		log.Debugln("Service Logentries mapped")
-		if *logentriesID == "" {
-			log.Fatal("Cannot specify both logentriesID")
-		}
-	} else if *service == "rapid7" {
-		log.Debugln("Service Rapid7 mapped")
-	} else {
-		log.Fatal("Service's does not mapped")
-	}
-
 	log.Infoln("Starting logentries_exporter", version.Info())
-	// Scraper AccountUsage
-	accountUsage := exporter.AccountGetUsage(*logentriesID, *apikey, *service)
-	prometheus.MustRegister(accountUsage)
 
+	// Scraper AccountUsage
+	prometheus.MustRegister(exporter.AccountGetUsage(*apikey))
+
+	// Scraper LogsUsage
+	prometheus.MustRegister(exporter.LogsGetUsage(*apikey))
+
+	// Scraper Exporter version
 	prometheus.MustRegister(version.NewCollector("logentries_exporter"))
 
 	// setup and start webserver
